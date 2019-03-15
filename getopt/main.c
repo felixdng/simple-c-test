8978aedda5c2ccf0f23f53cf39cd72ea9118525a
felixdng <felixddzq@gmail.com>
2019-03-15 12:28:41 +0800

    update main.c

--- getopt/main.c
+++ getopt/main.c
@@ -1,63 +1,79 @@
-#include <stdio.h>
-#include <stdlib.h>
+
+#include <stdio.h>     /* for printf */
+#include <stdlib.h>    /* for exit */
 #include <getopt.h>
-
-enum
+ 
+int
+main(int argc, char **argv)
 {
-	OPT_RET_HELP     = 0x01,
-	OPT_RET_DIREC    = 0x02,
-	OPT_RET_L2       = 0x04,
-	OPT_RET_L3       = 0x08,
-	OPT_RET_L4       = 0x10,
-	OPT_RET_SMAC     = 0x20,
-	OPT_RET_DMAC     = 0x40,
-	OPT_RET_VLAN     = 0x80,
-	OPT_RET_IVLAN    = 0x100,
-
-	OPT_RET_NUM
-} E_OPT_RET;
-static void showusage(void)
-{
-	printf("Usage : sock [options]\n");
-	printf("Options:\n");
-	printf(" -h, --help                              Print this message.\n");
-	printf(" -D, --direction                         IO direction: read or write\n");
-	printf(" --l2 [l2type],                           Protocol level 2, eg:mac,vlan...\n");
-	printf("     l2type:\n");
-	printf("       mac : --smac [smac] | --dmac [dmac]\n");
-	printf("       vlan: --vlan [id] --ivlan [id]\n");
-	printf(" --l3,                                    Protocol level 3, eg:ip...\n");
-	printf(" --l4,                                    Protocol level 4, eg:tcp,udp...\n");
-	printf("");
+    int c;
+    int digit_optind = 0;
+ 
+   while (1) {
+        int this_option_optind = optind ? optind : 1;
+        int option_index = 0;
+        static struct option long_options[] = {
+            {"add",     required_argument, 0,  0 },
+            {"append",  no_argument,       0,  0 },
+            {"delete",  required_argument, 0,  0 },
+            {"verbose", no_argument,       0,  0 },
+            {"create",  required_argument, 0, 'c'},
+            {"file",    required_argument, 0,  0 },
+            {0,         0,                 0,  0 }
+        };
+ 
+       c = getopt_long(argc, argv, "abc:d:012",
+                 long_options, &option_index);
+        if (c == -1)
+            break;
+ 
+       switch (c) {
+        case 0:
+            printf("option %s", long_options[option_index].name);
+            if (optarg)
+                printf(" with arg %s", optarg);
+            printf("\n");
+            break;
+ 
+       case '0':
+        case '1':
+        case '2':
+            if (digit_optind != 0 && digit_optind != this_option_optind)
+              printf("digits occur in two different argv-elements.\n");
+            digit_optind = this_option_optind;
+            printf("option %c\n", c);
+            break;
+ 
+       case 'a':
+            printf("option a\n");
+            break;
+ 
+       case 'b':
+            printf("option b\n");
+            break;
+ 
+       case 'c':
+            printf("option c with value '%s'\n", optarg);
+            break;
+ 
+       case 'd':
+            printf("option d with value '%s'\n", optarg);
+            break;
+ 
+       case '?':
+            break;
+ 
+       default:
+            printf("?? getopt returned character code 0%o ??\n", c);
+        }
+    }
+ 
+   if (optind < argc) {
+        printf("non-option ARGV-elements: ");
+        while (optind < argc)
+            printf("%s ", argv[optind++]);
+        printf("\n");
+    }
+ 
+   exit(EXIT_SUCCESS);
 }
-
-static int opt_ret;
-static struct option longOpts[] = {
-	{"help",      no_argument,       &opt_ret, OPT_RET_HELP},
-	{"direction", required_argument, &opt_ret, OPT_RET_DIREC},
-	{"l2",        required_argument, &opt_ret, OPT_RET_L2},
-	{"l3",        required_argument, &opt_ret, OPT_RET_L3},
-	{"l4",        required_argument, &opt_ret, OPT_RET_L4},
-	{"smac",      required_argument, &opt_ret, OPT_RET_SMAC},
-	{"dmac",      required_argument, &opt_ret, OPT_RET_DMAC},
-	{"vlan",      required_argument, &opt_ret, OPT_RET_VLAN},
-	{"ivlan",     required_argument, &opt_ret, OPT_RET_IVLAN},
-	{0, 0, 0, 0},
-};
-static int parse_parameter(int argc, char **argv)
-{
-	while (1)
-	{
-		int ret = getopt_long(argc, argv, "hD:", longOpts, NULL);
-		if (-1 == ret)
-			break;
-	}
-	return 0;
-}
-
-int main(int argc, char **argv)
-{
-	parse_parameter(argc, argv);
-	return 0;
-}
-
